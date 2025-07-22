415923e79a2bf8ddc774316ad3d3169c21e09a97
BhaveshMulchandani <bhaveshmulchandani330@gmail.com>
2025-07-22 15:41:16 +0530

    final product page done

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -10,26 +10,6 @@ const Product = () => {
       <Navbar />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 place-items-center p-10">
-        {/* pc-1 */}
-        <div className="max-w-sm bg-[#ffdbd4] rounded-2xl shadow-md p-6 text-[#6a4639] font-irish transition-transform duration-200 hover:scale-105">
-          <img
-            src={ghee}
-            alt="Gir Cow A2 Ghee"
-            className="w-full h-64 object-contain rounded-md mb-4"
-          />
-
-          <h1 className="text-[#3c2722] text-xl font-semibold mt-2 px-2">
-            Gir Cow A2 Ghee - Made With Curd
-          </h1>
-
-          <h4 className="text-[#49332d] font-medium text-lg  mb-6 px-3">
-            ₹ 1,799
-          </h4>
-
-          <button className="w-full py-2 rounded-md text-lg font-semibold border border-[#4a3127]  text-[#4a3127] hover:ring-1 hover:ring-[#4a3127] transition">
-            Add To Cart
-          </button>
-        </div>
 
         <div className="max-w-sm bg-[#ffdbd4] rounded-2xl shadow-md p-6 text-[#6a4639] font-irish transition-transform duration-200 hover:scale-105">
           <img
@@ -38,11 +18,11 @@ const Product = () => {
             className="w-full h-64 object-contain rounded-md mb-4"
           />
 
-          <h1 className="text-[#3c2722] text-2xl font-semibold mt-2 px-2">
+          <h1 className="text-[#3c2722] text-xl font-semibold mt-2 px-2">
             Gir Cow A2 Ghee - Made With Curd
           </h1>
 
-          <h4 className="text-[#49332d] font-medium text-xl  mb-6 px-3">
+          <h4 className="text-[#49332d] font-medium text-lg mb-6 px-3">
             ₹ 1,799
           </h4>
 
@@ -50,105 +30,6 @@ const Product = () => {
             Add To Cart
           </button>
         </div>
-
-        <div className="max-w-sm bg-[#ffdbd4] rounded-2xl shadow-md p-6 text-[#6a4639] font-irish transition-transform duration-200 hover:scale-105">
-          <img
-            src={ghee}
-            alt="Gir Cow A2 Ghee"
-            className="w-full h-64 object-contain rounded-md mb-4"
-          />
-
-          <h1 className="text-[#3c2722] text-2xl font-semibold mt-2 px-2">
-            Gir Cow A2 Ghee - Made With Curd
-          </h1>
-
-          <h4 className="text-[#49332d] font-medium text-xl  mb-6 px-3">
-            ₹ 1,799
-          </h4>
-
-          <button className="w-full py-2 rounded-md text-lg font-semibold border border-[#4a3127]  text-[#4a3127] hover:ring-1 hover:ring-[#4a3127] transition">
-            Add To Cart
-          </button>
-        </div>
-
-        <div className="max-w-sm bg-[#ffdbd4] rounded-2xl shadow-md p-6 text-[#6a4639] font-irish transition-transform duration-200 hover:scale-105">
-          <img
-            src={ghee}
-            alt="Gir Cow A2 Ghee"
-            className="w-full h-64 object-contain rounded-md mb-4"
-          />
-
-          <h1 className="text-[#3c2722] text-2xl font-semibold mt-2 px-2">
-            Gir Cow A2 Ghee - Made With Curd
-          </h1>
-
-          <h4 className="text-[#49332d] font-medium text-xl  mb-6 px-3">
-            ₹ 1,799
-          </h4>
-
-          <button className="w-full py-2 rounded-md text-lg font-semibold border border-[#4a3127]  text-[#4a3127] hover:ring-1 hover:ring-[#4a3127] transition">
-            Add To Cart
-          </button>
-        </div>
-
-        <div className="max-w-sm bg-[#ffdbd4] rounded-2xl shadow-md p-6 text-[#6a4639] font-irish transition-transform duration-200 hover:scale-105">
-          <img
-            src={ghee}
-            alt="Gir Cow A2 Ghee"
-            className="w-full h-64 object-contain rounded-md mb-4"
-          />
-
-          <h1 className="text-[#3c2722] text-2xl font-semibold mt-2 px-2">
-            Gir Cow A2 Ghee - Made With Curd
-          </h1>
-
-          <h4 className="text-[#49332d] font-medium text-xl  mb-6 px-3">
-            ₹ 1,799
-          </h4>
-
-          <button className="w-full py-2 rounded-md text-lg font-semibold border border-[#4a3127]  text-[#4a3127] hover:ring-1 hover:ring-[#4a3127] transition">
-            Add To Cart
-          </button>
-        </div>
-        <div className="max-w-sm bg-[#ffdbd4] rounded-2xl shadow-md p-6 text-[#6a4639] font-irish transition-transform duration-200 hover:scale-105">
-          <img
-            src={ghee}
-            alt="Gir Cow A2 Ghee"
-            className="w-full h-64 object-contain rounded-md mb-4"
-          />
-
-          <h1 className="text-[#3c2722] text-2xl font-semibold mt-2 px-2">
-            Gir Cow A2 Ghee - Made With Curd
-          </h1>
-
-          <h4 className="text-[#49332d] font-medium text-xl  mb-6 px-3">
-            ₹ 1,799
-          </h4>
-
-          <button className="w-full py-2 rounded-md text-lg font-semibold border border-[#4a3127]  text-[#4a3127] hover:ring-1 hover:ring-[#4a3127] transition">
-            Add To Cart
-          </button>
-        </div>
-        <div className="max-w-sm bg-[#ffdbd4] rounded-2xl shadow-md p-6 text-[#6a4639] font-irish transition-transform duration-200 hover:scale-105">
-          <img
-            src={ghee}
-            alt="Gir Cow A2 Ghee"
-            className="w-full h-64 object-contain rounded-md mb-4"
-          />
-
-          <h1 className="text-[#3c2722] text-2xl font-semibold mt-2 px-2">
-            Gir Cow A2 Ghee - Made With Curd
-          </h1>
-
-          <h4 className="text-[#49332d] font-medium text-xl  mb-6 px-3">
-            ₹ 1,799
-          </h4>
-
-          <button className="w-full py-2 rounded-md text-lg font-semibold border border-[#4a3127]  text-[#4a3127] hover:ring-1 hover:ring-[#4a3127] transition">
-            Add To Cart
-          </button>
-        </div>
-      </div>
 
       {/* <div className="max-w-sm  rounded-2xl shadow-lg font-irish px-4 py-6 hover:scale-105 transition-all duration-150">
         <img
@@ -169,6 +50,7 @@ const Product = () => {
         </button>
       </div>
       */}
+    </div>
     </div>
   );
 };
